8f367deec34210f3f710175ec92ef3b852dd85e7
udhayavana <udhayavana@gmail.com>
2023-10-29 21:03:01 +0530

    Random text and color change using interval

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState , useEffect} from 'react';
+// function changecolor(){
+   
+//   const[titles,setTitle] = useState(
+//     [{title:"Am Udhayavanan"},
+//     { title : "this is my first react app"},
+//     { title : "Hello world"}]
+//   );
+
+// }
+
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [textcolor, setTextColor] = useState("black"); 
+  const [content, setContent] = useState("I Am udhayavanan,"); 
+  useEffect(() => { 
+  
+        //Implementing the setInterval method 
+        const colors=["red","pink","green"];
+        const contentist=["I Am udhayavanan,","This is my react learning exercise","Let me say ", "hello react"];
+        const interval = setInterval(() => { 
+            
+            setCount(count + 1); 
+            let ind=Math.floor(Math.random() * 3);
+            setTextColor(colors[ind]);
+            setContent(contentist[count%4]);
+        }, 1000); 
+  
+        //Clearing the interval 
+        return () => clearInterval(interval); 
+    }, [count]); 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{color:textcolor}} > {content} </p>
       </header>
     </div>
   );
